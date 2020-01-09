f85975fe6d05b9d38023de4d616627322ad10ac9
Jad Chehimi <j.chehimi@EDU.epsb.ca>
2020-01-09 15:31:14 -0700

    functions

--- rotations.js
+++ rotations.js
@@ -1,37 +1,37 @@
-const rotations = 
+const rawRotations = 
 {
     "tops": [
-        "Twister<br>Top",
-        "Blue Bullet<br>Top",
-        "Howler<br>Top",
+        "Twister Top",
+        "Blue Bullet Top",
+        "Howler Top",
         "Kiosk",
-        "Sky Screamer<br>Top",
-        "Corkscrew<br>Top",
-        "Caribbean Crusier<br>Top",
+        "Sky Screamer Top",
+        "Corkscrew Top",
+        "Caribbean Crusier Top",
         "Caribbean Cove 1",
         "Caribbean Cove 2"
     ],
 
     "cyclone": [
-        "Cyclone<br>Top",
-        "Slideboarding<br>Top",
+        "Cyclone Top",
+        "Slideboarding Top",
         "Cyclone Middle",
-        "Twister<br>Bottom",
-        "Double<br> Hot Tubs",
-        "Caribbean Cruiser<br>Bottom",
-        "Howler<br>Bottom",
-        "Slideboarding<br>Bottom",
+        "Twister Bottom",
+        "Double  Hot Tubs",
+        "Caribbean Cruiser Bottom",
+        "Howler Bottom",
+        "Slideboarding Bottom",
         "Dolphin Kiddie Pool"
     ],
 
     "nessies": [
-        "Nessie's Revenge<br>Top",
-        "Tropical Typhoon<br>Top",
-        "Blue Bullet<br>Bottom",
-        "Corkscrew<br>Bottom",
-        "Sky Screamer<br>Bottom",
-        "Sunrunner<br>Top",
-        "Sunrunner<br>Bottom",
-        "Single<br> Hot Tub"
+        "Nessie's Revenge Top",
+        "Tropical Typhoon Top",
+        "Blue Bullet Bottom",
+        "Corkscrew Bottom",
+        "Sky Screamer Bottom",
+        "Sunrunner Top",
+        "Sunrunner Bottom",
+        "Single Hot Tub"
     ]
 };
--- script.js
+++ script.js
@@ -1,16 +1,58 @@
+// Get Data
+let getRotation = _ => {
+    return document.getElementById("rotations").value;
+}
 
+let getRotators = _ => {
+    return document.getElementById("rotators").value; //string
+}
 
-// Rotation Option Choice
-document.getElementById("rotations").addEventListener("change", _ => {
-    displayRotationOnTable(document.getElementById("rotations").value);
-});
+let getStartingSpot = _ => {
+    return document.getElementById("spots").value;
+}
+
+let getOpenTime = _ => {
+    let raw = document.getElementById("opening").value;
+    return moment(raw, "HH:mm");
+}
+
+let getCloseTime = _ => {
+    let raw = document.getElementById("closing").value;
+    return moment(raw, "HH:mm");
+}
 
 // Table Functions
-
-let displayRotationOnTable = rotation => {
+let currentRotation;
+let populateTable = _ => {
+    // Reset
     clearTable();
-    for(let i = 0; i < rotations[rotation].length; i++){
-        addToTable(Math.floor(Math.random() * 1000), rotations[rotation][i]);
+    setBreaks();
+    currentRotation = {};
+
+    let iterate = 0;
+    let open = getOpenTime();
+    let rotation = rotations[getRotation()];
+
+    // Update starting spot choice
+    for(let i = 0; i < rotation.length; i++){
+        let op = document.createElement("option");
+        op.innerHTML = rotation[i];
+        document.getElementById("spots").appendChild(op);
+    }
+
+    console.log(open.format("hh:mm a"));
+    console.log(getOpenTime().format("hh:mm a"));
+    console.log(getCloseTime().format("hh:mm a"));
+
+    while(open.isBefore(getCloseTime())){
+        currentRotation[open] = rotation[iterate];
+        addToTable(open.format("h:mm a"), rotation[iterate]);
+
+        // Check for break, if not normal add
+        open.add(20, "minutes");
+        
+        console.log(rotation.length + " --- " + iterate);
+        iterate = iterate < rotation.length - 1 ? iterate + 1 : 0
     }
 }
 
@@ -36,7 +78,32 @@ let clearTable = _ => {
     while(tbody.lastChild) tbody.removeChild(tbody.lastChild);
 }
 
+// Option Choice Updates Everything
+let inputs = document.querySelectorAll(".inp");
+inputs.forEach(i => {
+    i.addEventListener("change", populateTable);
+});
+
+// Add breaks
+let rotations;
+let setBreaks = _ => {
+    rotations = rawRotations;
+    for(let i = 0; i < getRotators; i++){
+        rotations[getRotation()].push("Break");
+    }
+}
+
+// Clock
+let updateClock = _ => {
+    let format = moment().format("h:mm:ss a");
+    document.getElementById("clock").innerHTML = format;
+    document.getElementById("currentspot").innerHTML = currentRotation[moment()];
+    //console.log(getCloseTime().diff(getOpenTime(), "minutes"))
+    setTimeout(updateClock, 100);
+}
+
 // DEFAULTs
 (_ => {
-    displayRotationOnTable("tops");
+    populateTable();
+    updateClock();
 })();
